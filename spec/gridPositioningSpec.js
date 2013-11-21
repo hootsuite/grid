36ab50b77fd60f57de1e0b26a6e6dcf30e8cee34
Ovidiu Chereches <skidding@gmail.com>
2013-11-21 17:21:41 +0200

    Add positioning test cases for new grid type

--- spec/gridPositioningSpec.js
+++ spec/gridPositioningSpec.js
@@ -1,371 +1,49 @@
 // Load dependencies when run with Node (in browser they are expected to
 // already be included)
 if (typeof(require) == 'function') {
-  var GridList = require('../lib/gridList.js').GridList;
+  var GridList = require('../lib/gridList.js').GridList,
+      fixtures = require('./fixtures.js');
 }
 
-describe("Grid positioning for", function() {
+describe("Grid positioning", function() {
 
-  var gridPositionOfWidgets = function(items, gridListOptions) {
-    var myGrid = new GridList(items, gridListOptions),
-        translatedItems = [],
-        i,
-        item,
-        group;
-    myGrid.generatePositionsFromIndex();
-    for (i = 0; i < items.length; i++) {
-      item = items[i];
-      group = Math.floor(item.position.col / myGrid.options.colsPerGroup);
-      // Create group if this is first item in it
-      if (!translatedItems[group]) {
-        translatedItems[group] = [];
+  beforeEach(function() {
+    this.addMatchers({
+      // We don't care about the other fields, only the positions and that the
+      // indexes correspond (e.g. the h changes for items with auto height)
+      toEqualPositions: function(expected) {
+        for (var i = 0; i < expected.length; i++) {
+          if (expected[i].x != this.actual[i].x ||
+              expected[i].y != this.actual[i].y) {
+            console.log(i, expected[i], this.actual[i]);
+            return false;
+          }
+        }
+        return true;
       }
-      translatedItems[group].push({
-        x: item.position.col % myGrid.options.colsPerGroup,
-        y: item.position.row
-      });
-    }
-    return translatedItems;
-  };
-
-  it("rightPositioning", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 2, rows: 1},
-      {cols: 2, rows: 1},
-      {cols: 1, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 0, y: 1},
-      {x: 2, y: 1}
-    ]]);
+    });
   });
 
-  it("longBarCharts", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 2},
-      {cols: 2, rows: 1},
-      {cols: 1, rows: 2}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 1, y: 1}
-    ]]);
-  });
+  describe("for grid configuration #1", function() {
 
-  it("placementAfterTimeline", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 1},
-      {cols: 1, rows: 1},
-      {cols: 1, rows: 4},
-      {cols: 1, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 2, y: 0}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
+    var gridFixture = fixtures.GRID1;
+    var grid = new GridList(GridList.cloneItems(gridFixture.rows3), {
+      rows: 3
+    });
 
-  it("timelineInNewGroup", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 1},
-      {cols: 2, rows: 1},
-      {cols: 1, rows: 4}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
+    it("should maintain positions for 3 rows", function() {
+      grid.createGrid(3);
+      expect(grid.items).toEqualPositions(gridFixture.rows3);
+    });
 
-  it("widget3xInNewGroup", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 2, rows: 1},
-      {cols: 2, rows: 1},
-      {cols: 2, rows: 1},
-      {cols: 1, rows: 1},
-      {cols: 1, rows: 1},
-      {cols: 3, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 0, y: 1},
-      {x: 0, y: 2},
-      {x: 2, y: 2},
-      {x: 0, y: 3}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
+    it("should generate correct positions for 2 rows", function() {
+      grid.createGrid(2);
+      expect(grid.items).toEqualPositions(gridFixture.rows2);
+    });
 
-  it("barchartWithMoreThan5Sources", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 1},
-      {cols: 1, rows: 2},
-      {cols: 3, rows: 1},
-      {cols: 1, rows: 2}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 0, y: 2}
-    ], [
-      {x: 0, y: 0}
-    ]]);
+    it("should generate correct positions for 4 rows", function() {
+      grid.createGrid(4);
+      expect(grid.items).toEqualPositions(gridFixture.rows4);
+    });
   });
-
-  it("barchartsGalore", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 2},
-      {cols: 1, rows: 2},
-      {cols: 1, rows: 2},
-      {cols: 3, rows: 1},
-      {cols: 3, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 2, y: 0},
-      {x: 0, y: 2},
-      {x: 0, y: 3}
-    ]]);
-  });
-
-  it("wideWidgetsAndTimeline", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 3, rows: 1},
-      {cols: 2, rows: 1},
-      {cols: 1, rows: 4}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 0, y: 1}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
-
-  it("wideWidgetInNewGroup", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 1},
-      {cols: 1, rows: 2},
-      {cols: 3, rows: 1},
-      {cols: 1, rows: 1},
-      {cols: 3, rows: 0}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 0, y: 2},
-      {x: 0, y: 3}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
-
-  it("emptySpaceFromWideAndTallWidgets", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 3, rows: 1},
-      {cols: 1, rows: 2},
-      {cols: 3, rows: 1},
-      {cols: 1, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 0, y: 1},
-      {x: 0, y: 3}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
-
-  it("twoTimelinesInGroup", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 4},
-      {cols: 1, rows: 4},
-      {cols: 1, rows: 1},
-      {cols: 1, rows: 1},
-      {cols: 2, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 2, y: 0},
-      {x: 2, y: 1}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
-
-  it("manyShortWidgetsInGroup", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 2, rows: 1},
-      {cols: 1, rows: 1},
-      {cols: 1, rows: 1},
-      {cols: 2, rows: 1},
-      {cols: 3, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 2, y: 0},
-      {x: 0, y: 1},
-      {x: 1, y: 1},
-      {x: 0, y: 2}
-    ]]);
-  });
-
-  it("emptyRowInGroup", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 3, rows: 1},
-      {cols: 1, rows: 2},
-      {cols: 2, rows: 1},
-      {cols: 3, rows: 1},
-      {cols: 1, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 0, y: 1},
-      {x: 1, y: 1},
-      {x: 0, y: 3}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
-
-  it("twoSquareWidgets", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 2, rows: 2},
-      {cols: 2, rows: 2}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 0, y: 2}
-    ]]);
-  });
-
-  it("emptyColumnInGroup", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 2, rows: 2},
-      {cols: 2, rows: 1},
-      {cols: 2, rows: 2},
-      {cols: 1, rows: 4}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 0, y: 2}
-    ], [
-      {x: 0, y: 0},
-      {x: 2, y: 0}
-    ]]);
-  });
-
-  it("smallAndBigWidgets", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 2, rows: 2},
-      {cols: 1, rows: 1},
-      {cols: 1, rows: 2},
-      {cols: 2, rows: 2},
-      {cols: 1, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 2, y: 0},
-      {x: 2, y: 1}
-    ], [
-      {x: 0, y: 0},
-      {x: 2, y: 0}
-    ]]);
-  });
-
-  it("widgetUnderEmptySpace", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 1},
-      {cols: 2, rows: 2},
-      {cols: 1, rows: 1}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 0, y: 2}
-    ]]);
-  });
-
-  it("allWidgetSizes", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 1},
-      {cols: 2, rows: 2},
-      {cols: 1, rows: 2},
-      {cols: 1, rows: 1},
-      {cols: 2, rows: 2}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 0, y: 2},
-      {x: 1, y: 2}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
-
-  it("bigBarchartInNewGroup", function() {
-    expect(gridPositionOfWidgets([
-      {cols: 1, rows: 4},
-      {cols: 1, rows: 1},
-      {cols: 2, rows: 2},
-      {cols: 1, rows: 2}
-    ], {
-      rows: 4,
-      colsPerGroup: 3
-    })).toEqual([[
-      {x: 0, y: 0},
-      {x: 1, y: 0},
-      {x: 1, y: 1}
-    ], [
-      {x: 0, y: 0}
-    ]]);
-  });
-
 });
