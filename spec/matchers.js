2481eb1f54a226b0b7f4c92bf5268f25c6dcc741
Ovidiu Chereches <skidding@gmail.com>
2013-11-26 11:18:38 +0200

    Include offending items in equalPosition matcher failing message

--- spec/matchers.js
+++ spec/matchers.js
@@ -3,10 +3,18 @@ exports.toEqualPositions = function(expected) {
     We don't care about the other fields, only the positions and that the
     indexes correspond (e.g. the h changes for items with auto height)
   */
+ var _expected,
+     _actual;
+ this.message = function () {
+    return JSON.stringify(_actual) +
+           " should be at position " +
+           JSON.stringify(_expected);
+  };
   for (var i = 0; i < expected.length; i++) {
     if (expected[i].x != this.actual[i].x ||
         expected[i].y != this.actual[i].y) {
-      console.log('Actual positions', expected[i], this.actual[i]);
+      _expected = expected[i];
+      _actual = this.actual[i];
       return false;
     }
   }
