13f8ec19a5e49b49913f84dec788b4c1a2e17c9b
mihaicc <caraman.mihai@gmail.com>
2014-04-03 19:00:07 +0300

    Enabled grid to print as string

--- spec/matchers.js
+++ spec/matchers.js
@@ -1,3 +1,7 @@
+if (typeof(require) == 'function') {
+  var GridList = require('../src/gridList.js');
+};
+
 exports.toEqualPositions = function(expected) {
   /**
     We don't care about the other fields, only the positions and that the
@@ -6,6 +10,15 @@ exports.toEqualPositions = function(expected) {
  var _expected,
      _actual;
  this.message = function () {
+    _expected.w = (_expected.w !== 'undefined' && _expected.w) || 1;
+    _expected.h = (_expected.h !== 'undefined' && _expected.h) || 1;
+    _actual.w = (_actual.w !== 'undefined' && _actual.w) || 1;
+    _actual.h = (_actual.h !== 'undefined' && _actual.h) || 1;
+ 
+    console.log('\n\n' +
+      'Expected: ' + GridList.renderItemsToString([_expected]) +
+      'Actual: ' + GridList.renderItemsToString([_actual]));
+
     return JSON.stringify(_actual) +
            " should be at position " +
            JSON.stringify(_expected);
