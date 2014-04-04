ce5a38afb22de78b6f0b0dd8ebbb41c8328d421d
mihaicc <caraman.mihai@gmail.com>
2014-04-04 13:20:56 +0300

    Added some comments to the jasmine custom matcher

--- spec/matchers.js
+++ spec/matchers.js
@@ -3,6 +3,7 @@ if (typeof(require) == 'function') {
 };
 
 _default_size = function(items) {
+  /* Set width and height of each item if it doesn't have one */
   for (var i = 0; i < items.length; i++ ) {
     var item = items[i];
     item.w = item.w !== undefined ? item.w : 1;
@@ -19,13 +20,17 @@ exports.toEqualPositions = function(expected) {
      _actual,
      actual = this.actual;
  this.message = function () {
-
+    /* To avoid writing in each test fully specified items for the grid to
+     * print them, we'll default sizes, allowing us to show to the developer
+     * from where the widget starts atleast.*/
+    // Print the grids
     _default_size(expected);
     _default_size(actual);
     console.log('\n\n' +
       'Expected: ' + (new GridList(expected, {rows: 5})) +
       'Actual: ' + (new GridList(actual, {rows: 5})));
 
+    // Let Jasmine know which are the items that differ
     return JSON.stringify(_actual) +
            " should be at position " +
            JSON.stringify(_expected);
