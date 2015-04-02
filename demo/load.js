2ce1dd3ed6aec2eeb620995bf59743c5a8029843
Andrei Picus <office.nightcrawler@gmail.com>
2015-04-02 11:28:50 +0300

    Add more resize controls.
    Add new vertical sizes.

--- demo/load.js
+++ demo/load.js
@@ -11,6 +11,8 @@ var DemoGrid = {
               '<a href="#zoom1" class="resize" data-w="1" data-h="1">1x1</a>' +
               '<a href="#zoom2" class="resize" data-w="2" data-h="1">2x1</a>' +
               '<a href="#zoom3" class="resize" data-w="3" data-h="1">3x1</a>' +
+              '<a href="#zoom1" class="resize" data-w="1" data-h="2">1x2</a>' +
+              '<a href="#zoom2" class="resize" data-w="2" data-h="2">2x2</a>' +
             '</div>' +
             i +
           '</div>' +
